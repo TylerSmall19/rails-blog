74952059a2a09b98ad916b997f7d88bf485a6707
Tyler Small <email": null>
2017-01-01 22:16:12 -0600

    Debugging save point for argument nil error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    Post.find_by(id: params[:id]).destroy
+    redirect_to '/'
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+    render 'update'
+  end
+
+  def patch
+    @post = Post.find_by(id: params[:id]) #.assign_attributes(post_params)
+    if @post && @post.save
+      render partial: 'post'
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
