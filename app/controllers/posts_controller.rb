9de663f37b4e658df67b8d76da719a1932e385f4
Tyler Small <email": null>
2017-01-01 21:05:33 -0600

    Add support for post submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.new
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc )
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to '/'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
